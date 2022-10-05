52371e880d4ee1b02a81f6d57e9f3d3126405828
jarora06 <105962875+jarora06@users.noreply.github.com>
2022-10-05 00:17:50 -0400

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+public class Rectangle {
+
+	public static void main(String[] args) {
+		
+
+	}
+	public void setFields(int newx, int newy, int newwidth, int newheight) {
+		Random rand = new Random();
+		int newx1 = rand.nextInt();
+		int newy1 = rand.nextInt();
+		int newwidht = rand.nextInt();
+		int newheight1 = rand.nextInt();
+	}
+	public int getHeight() {
+		
+	}
+	public int getWidth() {
+		
+	}
+	public int getX() {
+		
+	}
+	public int getY() {
+		
+	}
+	public String toString() {
+		
+	}
+
+}
+ 
---
+++ RectangleClient.java
@@ -0,0 +1,12 @@
+
+public class RectangleClient extends Rectangle{
+	public static void main(String[] args) {
+		Rectangle rec1 = new Rectangle();
+		Rectangle rec2 = new Rectangle();
+		rec1.setFields(5, 8, 10, 4); 
+		rec2.setFields(9, 3, 12, 21); 
+		System.out.println("Rectangle 1: "+ rec1); //prints out the first rectangle
+		System.out.println("Rectangle #2: "+ rec2); //prints out the second rectangle
+
+}
+}
